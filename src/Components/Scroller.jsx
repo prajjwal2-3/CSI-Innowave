c8412701383fa6a585c39fdc96cabc7bc60108bf
joyalljijo <joyal2405@gmail.com>
2024-02-21 22:30:23 +0530

    Optimised Scroller for mobile

--- src/Components/Scroller.jsx
+++ src/Components/Scroller.jsx
@@ -5,8 +5,11 @@ import "./Scroller.css"; // Import the CSS file for your animations
 const Scroller = () => {
     return (
         <div className="events-container p-5 bg-black">
-            <h2 className="text-5xl font-bold mb-5 text-center bg-black">Updates &amp; Events</h2>
-            <div className="events-scroll-container" >
+            <h2 className="font-bold mb-5 text-center bg-black text-3xl  md:text-4xl lg:text-5xl ">
+                Updates &amp; Events
+            </h2>
+
+            <div className="events-scroll-container">
                 {eventData.map((event) => (
                     <div
                         key={event.title}
@@ -15,11 +18,14 @@ const Scroller = () => {
                         <img
                             src={event.posterLink}
                             alt={event.title}
-                            className=" w-full h-30 object-cover mb-4 rounded-md"
+                            className="w-full h-30 object-cover mb-4 rounded-md sm:h-auto" // Responsive height for mobile
                         />
-                        <h3 className="text-black text-lg font-bold mb-2">{event.title}</h3>
-                        <p className="text-gray-500 mb-4">{event.shortDescription}</p>
-                        <p className="text-gray-500">{event.date}</p>
+                        {/* Conditional rendering for description on larger screens */}
+                        <div className="hidden sm:block"> {/* Hide on small screens */}
+                            <h3 className="text-black text-lg font-bold mb-2">{event.title}</h3>
+                            <p className="text-gray-500 mb-4">{event.shortDescription}</p>
+                            <p className="text-gray-500">{event.date}</p>
+                        </div>
                     </div>
                 ))}
             </div>
