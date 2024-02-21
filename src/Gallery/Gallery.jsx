79ef78604237348ec49d70b4ea8ca6581ab62d49
joyalljijo <joyal2405@gmail.com>
2024-02-21 19:17:53 +0530

    created gallery page

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import galleryData from "./GalleryData.jsx"; // Import your gallery data
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+    return (
+        <div className="grid grid-cols-3 gap-4 m-5">
+            {galleryData.map((image) => (
+                <div
+                    key={image.title}
+                    className="bg-white p-4 shadow-md rounded-sm m-2 h-96"
+                >
+                    <img
+                        src={image.imageLink}
+                        alt={image.title}
+                        className="w-full h-full object-cover mb-4 rounded-md"
+                    />
+                </div>
+            ))}
+        </div>
+    );
 };
 
 export default Gallery;
---
+++ src/Gallery/GalleryData.jsx
@@ -0,0 +1,40 @@
+const galleryData = [
+    {
+        title: "one",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521009/CSI%20Website/Gallery/mlduoo38gu0h85stcpdk.jpg"
+    },
+    {
+        title: "two",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521035/CSI%20Website/Gallery/iuved39sv3qpctjmn63h.jpg"
+    },
+    {
+        title: "three",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521349/CSI%20Website/Gallery/tal12kplyxflg5tdti3x.jpg"
+    },
+    {
+        title: "four",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521349/CSI%20Website/Gallery/r4vsj5zgnb4vhuq1vseu.jpg"
+    },
+    {
+        title: "five",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521349/CSI%20Website/Gallery/f7n7ssq7ggdnf1mp2bfj.jpg"
+    },
+    {
+        title: "six",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521349/CSI%20Website/Gallery/vt0q1yxcwletk1c2l1wg.jpg"
+    },
+    {
+        title: "seven",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521350/CSI%20Website/Gallery/jjlaeehn9s0dupruqinw.jpg"
+    },
+    {
+        title: "eight",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708521350/CSI%20Website/Gallery/qtgigrazkp8vgohawxtm.jpg"
+    },
+    {
+        title: "nine",
+        imageLink: "https://res.cloudinary.com/dfhj4i9hd/image/upload/v1708522996/CSI%20Website/Gallery/g0oixaysfo22yqqeu9px.jpg"
+    }
+];
+
+export default galleryData;
