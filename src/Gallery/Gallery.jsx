73c7960e5e43ddbe2bb040a341b49820a1e17022
Yash <yg232004@gmail.com>
2024-02-25 00:07:44 +0530

    fixes

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -1,22 +1,22 @@
 import galleryData from "./GalleryData.jsx"; // Import your gallery data
 
 const Gallery = () => {
-    return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 m-5">
-            {galleryData.map((image) => (
-                <div
-                    key={image.title}
-                    className="bg-white p-4 shadow-md rounded-sm m-2 h-96"
-                >
-                    <img
-                        src={image.imageLink}
-                        alt={image.title}
-                        className="w-full h-full  mb-4 rounded-md object-contain"
-                    />
-                </div>
-            ))}
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 m-5">
+      {galleryData.map((image) => (
+        <div
+          key={image.title}
+          className="bg-white p-1 shadow-md rounded-sm m-2 h-96"
+        >
+          <img
+            src={image.imageLink}
+            alt={image.title}
+            className="w-full h-full  mb-4 rounded-md object-contain"
+          />
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Gallery;
