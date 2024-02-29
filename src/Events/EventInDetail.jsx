1ad488516152b81d01f08ec49e50d7028ec5c379
Yash <yg232004@gmail.com>
2024-02-29 19:27:30 +0530

    fixed event in detail

--- src/Events/EventInDetail.jsx
+++ src/Events/EventInDetail.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 
 const EventInDetail = () => {
   const { id } = useParams();
-  const event = eventData.find((event) => event.id === id);
+  const event = eventData.find((event) => event.id == id);
 
   if (!event) {
     return <div>Event not found</div>;
